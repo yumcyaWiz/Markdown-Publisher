34ea4ff146bf7d15e69582dfaf93164109d33a37
yumcyawiz <sergeyinverse@gmail.com>
2018-09-04 01:47:57 +0900

    グリッド分割

--- src/App.vue
+++ src/App.vue
@@ -18,3 +18,40 @@ export default {
   }
 }
 </script>
+
+<style>
+html, body, #app {
+  margin: 0;
+  width: 100%;
+  height: 100%;
+  font-family: 'Helvetica Neue', Arial, sans-serif;
+  color: #333;
+  display: flex;
+}
+
+textarea, #editor, #viewer {
+  width: 100%;
+  height: 100%;
+  vertical-align: top;
+  box-sizing: border-box;
+}
+#viewer {
+  padding: 0 20px;
+}
+
+textarea {
+  border: none;
+  border-right: 1px solid #ccc;
+  resize: none;
+  outline: none;
+  background-color: #f6f6f6;
+  font-size: 14px;
+  font-family: 'Monaco', courier, monospace;
+  padding: 20px;
+}
+
+pre {
+  background: #EEE;
+  padding: 10px 10px;
+}
+</style>
